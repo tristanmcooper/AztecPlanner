a2d2ae553098d6c8267e5b41a1a3ce6cbc2fbf2d
MaximPodgore <56902053+MaximPodgore@users.noreply.github.com>
2025-11-16 14:27:00 -0800

    semester planner doesnt requery + objects selectable

--- front-end-webserver/src/assets/prompts.js
+++ front-end-webserver/src/assets/prompts.js
@@ -1,6 +1,6 @@
 export const prompts = {
     START: "Hi {name}, we'll start planning out a semester of your choosing",
-    CALCULATE_REQS: "We have looked at the requirements you've completed so far, consulted the SDSU CS requirements and roadmap, and detmermined the requirements you need to take next.",
+    CALCULATE_REQS: "We have looked at the requirements you've completed so far, consulted the SDSU CS requirements and roadmap, and determined the requirements you need to take next.",
     SUGGEST_COURSES: "Consulting the SDSU course catalog + rateMyProfessor data to find the best rated courses that fit the requirement",
     CONFIRM: "Now click the confirm button to add these courses to your plan!",
 }
